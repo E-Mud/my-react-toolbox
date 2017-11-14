8c125acec2b0afe7ad85404b8a60fb645a72af5f
E-Mud <better.acs@gmail.com>
2017-11-14 11:29:43 +0100

    Buttons mixinized

--- stories/elements/buttons.jsx
+++ stories/elements/buttons.jsx
@@ -1,28 +1,68 @@
 import React from 'react';
 
+import StoryContainer from '../StoryContainer.jsx'
 import { storiesOf } from '@storybook/react';
 
+const buttonStyle = {marginRight: '10px'}
+
 storiesOf('Button', module)
-    .add('Regular', () => {
+    .add('Text', () => {
         return (
             <div>
-                <div className="flex-container" style={{padding: '10px'}}>
-                    <button style={{marginRight: '10px'}}>NORMAL</button>
-                    <button style={{marginRight: '10px'}} disabled={true}>DISABLED</button>
-                </div>
-                <div className="flex-container" style={{padding: '10px'}}>
-                    <button className="primary" style={{marginRight: '10px'}}>NORMAL</button>
-                    <button className="primary" style={{marginRight: '10px'}} disabled={true}>DISABLED</button>
-                </div>
+                <StoryContainer className="flex-container" padding="large">
+                    <button style={buttonStyle}>NORMAL</button>
+                    <button style={buttonStyle} disabled={true}>DISABLED</button>
+                </StoryContainer>
+                <StoryContainer className="flex-container" padding="large">
+                    <button className="primary" style={buttonStyle}>NORMAL</button>
+                    <button className="primary" style={buttonStyle} disabled={true}>DISABLED</button>
+                </StoryContainer>
+                <StoryContainer className="flex-container" padding="large" theme="light">
+                    <button style={buttonStyle}>NORMAL</button>
+                    <button style={buttonStyle} disabled={true}>DISABLED</button>
+                </StoryContainer>
+                <StoryContainer className="flex-container" padding="large" theme="light">
+                    <button className="primary" style={buttonStyle}>NORMAL</button>
+                    <button className="primary" style={buttonStyle} disabled={true}>DISABLED</button>
+                </StoryContainer>
+                <StoryContainer className="flex-container" padding="large" theme="dark">
+                    <button style={buttonStyle}>NORMAL</button>
+                    <button style={buttonStyle} disabled={true}>DISABLED</button>
+                </StoryContainer>
+                <StoryContainer className="flex-container" padding="large" theme="dark">
+                    <button className="primary" style={buttonStyle}>NORMAL</button>
+                    <button className="primary" style={buttonStyle} disabled={true}>DISABLED</button>
+                </StoryContainer>
             </div>
         )
     })
-    .add('Raised', () => {
+    .add('Solid', () => {
         return (
-            <div className="flex-container" style={{padding: '10px'}}>
-                <button className="raised primary" style={{marginRight: '10px'}}>NORMAL</button>
-                <button className="filled primary" style={{marginRight: '10px'}}>NO DEPTH</button>
-                <button className="raised primary" style={{marginRight: '10px'}} disabled={true}>DISABLED</button>
+            <div>
+                <StoryContainer className="flex-container" padding="large">
+                    <button className="raised primary" style={buttonStyle}>NORMAL</button>
+                    <button className="raised primary" style={buttonStyle} disabled={true}>DISABLED</button>
+                </StoryContainer>
+                <StoryContainer className="flex-container" padding="large">
+                    <button className="raised warn" style={buttonStyle}>NORMAL</button>
+                    <button className="raised warn" style={buttonStyle} disabled={true}>DISABLED</button>
+                </StoryContainer>
+                <StoryContainer className="flex-container" padding="large" theme="light">
+                    <button className="raised primary" style={buttonStyle}>NORMAL</button>
+                    <button className="raised primary" style={buttonStyle} disabled={true}>DISABLED</button>
+                </StoryContainer>
+                <StoryContainer className="flex-container" padding="large" theme="light">
+                    <button className="raised warn" style={buttonStyle}>NORMAL</button>
+                    <button className="raised warn" style={buttonStyle} disabled={true}>DISABLED</button>
+                </StoryContainer>
+                <StoryContainer className="flex-container" padding="large" theme="dark">
+                    <button className="raised primary" style={buttonStyle}>NORMAL</button>
+                    <button className="raised primary" style={buttonStyle} disabled={true}>DISABLED</button>
+                </StoryContainer>
+                <StoryContainer className="flex-container" padding="large" theme="dark">
+                    <button className="raised warn" style={buttonStyle}>NORMAL</button>
+                    <button className="raised warn" style={buttonStyle} disabled={true}>DISABLED</button>
+                </StoryContainer>
             </div>
         )
     });
