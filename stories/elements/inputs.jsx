4329776ca4888a4322d6564bab5fb6742b6a8d21
E-Mud <better.acs@gmail.com>
2017-11-14 12:54:26 +0100

    Inputs mixinized

--- stories/elements/inputs.jsx
+++ stories/elements/inputs.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
 
+import StoryContainer from '../StoryContainer.jsx'
 import { storiesOf } from '@storybook/react';
 
 storiesOf('Input', module)
     .add('Single-line text', () => {
         return (
-            <div className="padding-base flex-container">
-                <div className="flex-20">
-                    <input type="text" placeholder="Placeholder" spellCheck="false"></input>
-                    <input type="text" placeholder="Invalid" spellCheck="false" className="invalid"></input>
-                    <input type="text" placeholder="Disabled" spellCheck="false" disabled="true"></input>
-                </div>
+            <div>
+                <StoryContainer className="flex-container" padding="large">
+                    <div className="flex-20">
+                        <input type="text" placeholder="Placeholder" spellCheck="false"></input>
+                        <input type="text" placeholder="Invalid" value={'Invalid value'} spellCheck="false" className="invalid"></input>
+                        <input type="text" placeholder="Invalid" spellCheck="false" className="invalid"></input>
+                        <input type="text" placeholder="Disabled" value={'Disabled value'} spellCheck="false" disabled="true"></input>
+                        <input type="text" placeholder="Disabled" spellCheck="false" disabled="true"></input>
+                    </div>
+                </StoryContainer>
+                <StoryContainer className="flex-container" padding="large" theme="light">
+                    <div className="flex-20">
+                        <input type="text" placeholder="Placeholder" spellCheck="false"></input>
+                        <input type="text" placeholder="Invalid" value={'Invalid value'} spellCheck="false" className="invalid"></input>
+                        <input type="text" placeholder="Invalid" spellCheck="false" className="invalid"></input>
+                        <input type="text" placeholder="Disabled" value={'Disabled value'} spellCheck="false" disabled="true"></input>
+                        <input type="text" placeholder="Disabled" spellCheck="false" disabled="true"></input>
+                    </div>
+                </StoryContainer>
+                <StoryContainer className="flex-container" padding="large" theme="dark">
+                    <div className="flex-20">
+                        <input type="text" placeholder="Placeholder" spellCheck="false"></input>
+                        <input type="text" placeholder="Invalid" value={'Invalid value'} spellCheck="false" className="invalid"></input>
+                        <input type="text" placeholder="Invalid" spellCheck="false" className="invalid"></input>
+                        <input type="text" placeholder="Disabled" value={'Disabled value'} spellCheck="false" disabled="true"></input>
+                        <input type="text" placeholder="Disabled" spellCheck="false" disabled="true"></input>
+                    </div>
+                </StoryContainer>
             </div>
         )
     })
